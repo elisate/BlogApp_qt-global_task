ac6e7f9d04c3aec0c6802a4c5834a4e3673c3796
elisate <dushimiyimanaelisa@gmail.com>
2024-08-05 05:03:11 +0200

    fix final deplyoyment

--- src/models/index.js
+++ src/models/index.js
@@ -4,12 +4,6 @@ import {sequelize} from '../../config/config.js'
 import { Comment } from "./comment.js";
 import { User } from "./user.js";
 
-// const sequelize = new Sequelize(process.env.POSTGRES_URI, {
-//   dialect: 'postgres',
-//   logging: false, // Set to true if you want to log SQL queries
-// });
-// Initialize models
-// Import models
 const models = {
   User,
   Post,
